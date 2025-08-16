6e95fff3c8cdfc44b6ef048a808fff88dad93719
nowa75 <nowa75@gmail.com>
2025-08-16 12:10:22 +0200

    45. Enabling static files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/nowa75/bookings/pkg/config"
 	"github.com/nowa75/bookings/pkg/handlers"
-	"net/http"
 )
 
 func routes(app *config.AppConfig) http.Handler {
@@ -22,6 +23,8 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 	fs := http.FileServer(http.Dir("./css"))
 	mux.Handle("/css/*", http.StripPrefix("/css", fs))
 	fsj := http.FileServer(http.Dir("./js"))
